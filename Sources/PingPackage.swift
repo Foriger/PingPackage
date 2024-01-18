0e0c5e70b1a39e4e9c6dff0a674dba3b793a22c5
radoslav.penev <radoslav.penev@miracl.com>
2024-01-18 19:55:50 +0200

    Add document parser arguments

--- Sources/PingPackage.swift
+++ Sources/PingPackage.swift
@@ -4,8 +4,15 @@ import Foundation
 
 @main
 struct PingPackage: ParsableCommand {
+    
+    @Option(name: .shortAndLong, help: "Port on which the `RadoSmallServer` will run.Default: 127.0.0.1")
+    var host = "127.0.0.1"
+    
+    @Option(name: .shortAndLong, help: "Host on which the `RadoSmallServer` will run.Default: 17443")
+    var port: Int = 17443
+    
     mutating func run() throws {
-        try RadoSmallServer(path: "/ping", method: .GET, type: .text) {
+        try RadoSmallServer(path: "/ping", method: .GET, type: .text, port: port) {
             Thread.sleep(forTimeInterval: Double.random(in: 0.01...10.0))
             return "OK".data(using: .utf8)!
         }.start()
